7b5508b8d91b9414fe5436c7fa569fa0ac6365e9
Joel Angel <joelbaraka4325@gmail.com>
2022-05-31 10:54:10 +0300

    changed the default app js

---
+++ components/Liked.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Liked = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default Liked;
+
+const styles = StyleSheet.create({});
