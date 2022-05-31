3725dfc68ec196abfd7ceb62baba3db97c1e8bc7
Joel Angel <joelbaraka4325@gmail.com>
2022-05-31 13:48:41 +0300

    creating the details page

--- components/Details.js
+++ components/Details.js
@@ -1,14 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import colors from '../assets/colors/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+const height = Dimensions.get('window').height;
 
 const Details = ({navigation, route}) => {
+  const {item} = route.params;
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={item.imageBig}
+        style={styles.backgroundImage}></ImageBackground>
+      <View style={styles.descriptionWrapper}></View>
     </View>
   );
 };
 
 export default Details;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  backgroundImage: {
+    // height: height, //this makes fullscreen image
+    height: height * 0.6, //this makes fullscreen image
+    justifyContent: 'space-between',
+  },
+  descriptionWrapper: {
+    flex: 1,
+    backgroundColor: colors.white,
+    marginTop: -20,
+    borderRadius: 25,
+  },
+});
