fbd66d7c5f06d0bd9c40e9ef6a9557dd60c044b9
Joel Angel <joelbaraka4325@gmail.com>
2022-05-31 14:14:25 +0300

    description text wrapper styled

--- components/Details.js
+++ components/Details.js
@@ -5,8 +5,8 @@ import {
   StyleSheet,
   ImageBackground,
   Dimensions,
+  TouchableOpacity,
 } from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
 import colors from '../assets/colors/colors';
 import Entypo from 'react-native-vector-icons/Entypo';
 
@@ -16,10 +16,29 @@ const Details = ({navigation, route}) => {
   const {item} = route.params;
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={item.imageBig}
-        style={styles.backgroundImage}></ImageBackground>
-      <View style={styles.descriptionWrapper}></View>
+      <ImageBackground source={item.imageBig} style={styles.backgroundImage}>
+        <TouchableOpacity
+          style={styles.backIcon}
+          onPress={() => navigation.goBack()}>
+          <Entypo name="chevron-left" size={32} color={colors.white} />
+        </TouchableOpacity>
+        <View style={styles.titlesWrapper}>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          <View style={styles.locationWrapper}>
+            <Entypo name="location-pin" size={24} color={colors.white} />
+            <Text style={styles.locationText}>{item.location}</Text>
+          </View>
+        </View>
+      </ImageBackground>
+      <View style={styles.descriptionWrapper}>
+        <View style={styles.heartWrapper}>
+          <Entypo name="heart" size={32} color={colors.orange} />
+        </View>
+        <View style={styles.descriptionTextWrapper}>
+          <Text style={styles.descriptionTitle}>Description</Text>
+          <Text style={styles.descriptionText}>{item.description}</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -41,5 +60,70 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     marginTop: -20,
     borderRadius: 25,
+  },
+
+  backIcon: {
+    marginLeft: 20,
+    marginTop: 60,
+  },
+
+  titlesWrapper: {
+    marginHorizontal: 20,
+    marginBottom: 40,
+  },
+  itemTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 32,
+    color: colors.white,
+  },
+
+  locationWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+
+  locationText: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 16,
+    color: colors.white,
+  },
+
+  heartWrapper: {
+    position: 'absolute',
+    top: -30,
+    right: 40,
+    width: 64,
+    height: 64,
+    backgroundColor: colors.white,
+    borderRadius: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  descriptionTextWrapper: {
+    marginTop: 30,
+    marginHorizontal: 20,
+  },
+
+  descriptionTitle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 24,
+    color: colors.black,
+  },
+  descriptionText: {
+    marginTop: 20,
+    fontFamily: 'Lato-Regular',
+    fontSize: 16,
+    color: colors.darkGray,
+    height: 85,
   },
 });
